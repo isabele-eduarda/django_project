0fb9d2b487bb790d4f3a623b025f1259e859476e
isabele-eduarda <isabeleeduarda307@gmail.com>
2026-04-15 13:00:51 -0300

    feat: interface home(teste), testando nova interface carrinho(ajustar)

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('home/', include('home.urls')),
+    path('carrinho/', include('home.urls')),
     path('admin/', admin.site.urls),
+    
 ]
